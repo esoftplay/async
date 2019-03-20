c55fa1ed20025e5e616850d3486d41b3a3028297
Danang Widiantoro <danang@fisip.net>
2019-03-20 11:12:21 +0700

    bugfix single quote

--- workers/esoftplay_async.php
+++ workers/esoftplay_async.php
@@ -3,7 +3,7 @@
 $mgr->addHandler('esoftplay_async', function ($string, GearmanJob $job) {
 	if (file_exists('/var/www/html/master/includes/class/async.php'))
 	{
-		$string = str_replace("'", "\'", $string);
+		$string = str_replace("'", "&#39;", $string);
 		shell_exec(PHP_BINARY.' /var/www/html/master/includes/class/async.php \''.$string.'\'');
 	}
 });

4f4a34975bf2c28bf4451016448dac81fb540888
Danang Widiantoro <danang@fisip.net>
2022-05-30 08:26:25 +0700

    replace new line in php command

--- workers/esoftplay_async.php
+++ workers/esoftplay_async.php
@@ -4,6 +4,7 @@ $mgr->addHandler('esoftplay_async', function ($string, GearmanJob $job) {
 	if (file_exists('/var/www/html/master/includes/class/async.php'))
 	{
 		$string = str_replace("'", "&#39;", $string);
+		$string = str_replace("\n", "\\\n", $string);
 		shell_exec(PHP_BINARY.' /var/www/html/master/includes/class/async.php \''.$string.'\'');
 	}
 });
